14bb5003207b24bcf9c208bdf067dbfb1ebfce38
404coderrr <rk8054988@gmail.com>
2026-01-25 13:36:50 +0530

    fix path

--- src/component/About.jsx
+++ src/component/About.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import AboutImg from "../assets/images/About.webp";
+import AboutImg from "../assets/images/about.webp";
 import {Link} from 'react-router-dom'
 
  function About() {
